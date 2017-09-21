d6483b66658c2b17ad1973549bc2450c8c650ecb
Alexis <afrz.code@gmail.com>
2017-09-21 13:56:25 +0200

    initial commit with styled component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Board from "./components/Board";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Board />
+      </div>
+    );
+  }
+}
