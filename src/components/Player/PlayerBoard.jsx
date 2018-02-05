a0d31c028de5860a32d2c23b820b5640ddd61491
afrz <afrz@users.noreply.github.com>
2018-02-05 13:57:36 +0100

    WIP family/player board

--- src/components/Player/PlayerBoard.jsx
+++ src/components/Player/PlayerBoard.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 import styled from "styled-components";
 
+import { getFamilyId, getId } from "../../logic/selectors";
+import { familySearcher } from "../../logic/static";
+
 const PlayerBoardContainer = styled.div`
   background: white;
-  border: 1px dashed gray;
   width: 100%;
   margin: 10px;
 `;
 
 const PlayerBoard = ({ cards }) => {
+  const byFamily = cards.reduce((acc, c) => {
+    const fid = getFamilyId(c);
+    if (!acc[fid]) acc[fid] = [];
+
+    acc[fid].push(c);
+    return acc;
+  }, {});
+
   return (
     <PlayerBoardContainer>
-      {cards.map(x => <div key={x._id}>{x.name}</div>)}
+      {Object.keys(byFamily).map(fid => {
+        const fam = familySearcher(fid);
+        return <FamilyThumb key={fid} family={fam} cards={byFamily[fid]} />;
+      })}
     </PlayerBoardContainer>
   );
+};
+
+const FamilyThumbContainer = styled.div`
+  border: 1px solid black;
+  margin: 10px 3px;
+  background-color: ${props => props.backcolor};
+`;
+
+const FamilyThumb = ({ family, cards }) => {
+  if (cards.length === 0) return null;
+
+  return (
+    <FamilyThumbContainer backcolor={family.color}>
+      <div>{family.company}</div>
+      {cards.map(x => <CardThumb key={getId(x)} card={x} />)}
+    </FamilyThumbContainer>
+  );
+};
+
+const CardThumbContainer = styled.div``;
+
+const CardThumb = ({ card }) => {
+  return <CardThumbContainer>{card.name.substring(0, 1)}</CardThumbContainer>;
 };
 
 export default PlayerBoard;
