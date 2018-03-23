0786cb72264540bab93905e1137693a43b62b31a
Alexis <afrz.code@gmail.com>
2018-03-23 13:17:38 +0100

    Updates

--- src/constants/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const PICK_CARD = "PICK_CARD";
-
-export const EMPTY_CARD = "empty";
--- src/logic/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import master from "./master";
-import board from "./board";
-
-export default combineReducers({
-  master,
-  board
-});
